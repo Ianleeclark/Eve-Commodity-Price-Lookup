15fda175df971898f9ba965c68723dd08dd25232
Ian Clark <ilc.ianclark@gmail.com>
2015-04-12 14:16:31 -0500

    CostAnalysis partially more complete. Needs to now calculate volume and total investment cost

--- eve_lookup/CostAnalysis.py
+++ eve_lookup/CostAnalysis.py
@@ -1,42 +1,45 @@
 from PriceHandling import item_price
-from math import e as e_constt
+from math import e as e_const
+import PlayerStats
 
-def transaction_tax(buy_price, player_stats):
+def shipping_cost(shipping):
+    origin, destination = shipping[0], shipping[1]
+    #               Jita      Rens      Dod        Amarr  
+    cost_matrix = [[0       , 26500000, 16500000, 10500000],
+                   [26500000, 0       , 15500000, 21500000],
+                   [16500000, 15500000, 0       , 17500000],
+                   [10500000, 21500000, 17500000, 0]]
+
+    return cost_matrix[origin][destination]
+
+
+def transaction_tax(buy_price, origin, player_stats):
+    """
+    Needs to lookup origin within PlayerStats.corp_stand
+    and correctly identify Jita:whatever location id
+    """
     p = player_stats
     
     broker_fee  = (buy_price * ((.01 - p.relations * 0.0005) / 
-                  (e_const**(p.fact_stand * 0.1 + p.corp_stand * 0.04))))
+                  (e_const**(p.fact_stand[origin] * 0.1 +
+                             p.corp_stand[origin] * 0.04))))
     sales_tax   = (buy_price * (.015 - (.0015 * 4)))
     
     return sales_tax + broker_fee
 
-def total_cost(buy_price, player_stats=None, shipping=None):
-    if shipping is None:
-        return buy_price + transaction_tax(buy_price)
+def total_cost(buy_price, shipping, player_stats=None):
+    if shipping[0] == shipping[1]:
+        return buy_price + transaction_tax(buy_price, None, player_stats)
     
-    # For the time being, we're assuming a Jita origin because it just doesn't 
-    # matter for what I'm currently trying to do with the project.
-    # However, this will be adjusted later.
-    #               Jita      Rens      Dod        Amarr  
-    cost_matrix = [0       , 26500000, 16500000, 10500000,
-                   26500000, 0       , 15500000, 21500000,
-                   16500000, 15500000, 0       , 17500000,
-                   10500000, 21500000, 17500000, 0]
-    
-    temp_cost = {"Jita": 0, "Rens": 26500000,
-                 "Dodixie": 16500000, "Amarr": 10500000}
-    origin = "Jita"
-    shipping_cost = temp_cost.get(shipping[1])
-    
-    return buy_price + shipping_cost + transaction_tax(buy_price, player_stats)
+    return buy_price + shipping_cost(shipping) + transaction_tax(buy_price,
+                                                                 shipping[0],
+                                                                 player_stats)
 
-def calculate_profit(prices, shipping=None):
-    cost = total_cost(prices[0], shipping)
-    if shipping is None:
-        pass
+def calculate_profit(prices, shipping):
+    cost = total_cost(prices[0], shipping, player_stats)
     return prices[1] - cost
     
 if __name__ == "__main__":
-    player_stats = PlayerStats(1.00, 5.00, 4, 4)
-    prices = item_price(17736, "Jita")
-    print calculate_profit(prices, player_stats["Jita", "Amarr"])
+    player_stats = PlayerStats(111, "1111")
+    prices = item_price(17736, ["Jita", "Amarr"])
+    print calculate_profit(prices, player_stats)
